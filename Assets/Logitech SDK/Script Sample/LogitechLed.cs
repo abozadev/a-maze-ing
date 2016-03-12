f61dbd9dd2808d061a829ebf28f2d74f689cbb78
August Boza <abozadev@gmail.com>
2016-03-12 12:28:09 +0100

    Update files

--- Assets/Logitech SDK/Script Sample/LogitechLed.cs
+++ Assets/Logitech SDK/Script Sample/LogitechLed.cs
@@ -16,66 +16,39 @@ public class LogitechLed : MonoBehaviour {
 	void Start () {
 
 		blue = 0;
-		red = 0;
+		red = 100;
 		green = 0;
 		LogitechGSDK.LogiLedInit();
 		LogitechGSDK.LogiLedSaveCurrentLighting();
-		effectLabel = "Press F to test flashing effect, P to test pulsing effect \n " +
-			"Press mouse1 to set all lighting to random color, mouse 2 to set G910 to random bitmap \n" +
-            "Press E to start per-key effects (F1-F12) show on supported devices \n" +
-			"Press S to stop the effects \n";
-	}
-	void OnGUI(){
-		GUI.Label(new Rect(10, 250, 500, 200), effectLabel);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        int duration = GetDlgItemInt(IDC_EDIT_DURATION, 0, 0);
-
-        int redVal = 0;
-        int greenVal = 0;
-        int blueVal = 0;
-        GetEffectColorValues(&redVal, &greenVal, &blueVal, true);
-
-        int redFinishVal = 0;
-        int greenFinishVal = 0;
-        int blueFinishVal = 0;
-        int duration1 = 1000;
-
-        GetEffectColorValues(&redFinishVal, &greenFinishVal, &blueFinishVal, false);
-        int loopChecked = m_perKeyPulseLoop.GetCheck();
 
 
-        LogiLedPulseSingleKey(LogiLed::KeyName::N, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::B, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::H, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::J, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::G, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::Y, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::U, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::I, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::T, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::SIX, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::EIGHT, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration1, loopChecked);
+        int finishRed = 0;
+        int finishBlue = 0;
+        int finishGreen = 0;
 
-        /*/
-        LogiLedPulseSingleKey(LogiLed::KeyName::N, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::B, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::H, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::J, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::G, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::Y, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::U, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::I, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::T, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::SIX, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        LogiLedPulseSingleKey(LogiLed::KeyName::EIGHT, redFinishVal, greenFinishVal, blueFinishVal, redVal, greenVal, blueVal, duration, loopChecked);
-        */
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.N, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.B, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.H, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.J, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.G, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.T, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.Y, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.U, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.I, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.SIX, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+        LogitechGSDK.LogiLedPulseSingleKey(LogitechGSDK.keyboardNames.EIGHT, finishRed, finishGreen, finishBlue, red, green, blue, 1000, true);
+
+        LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(LogitechGSDK.keyboardNames.A, 0, 100, 0);
     }
 
-    void OnDestroy () {
+
+    // Update is called once per frame
+    void Update () {
+
+        
+	}
+	
+	void OnDestroy () {
 		//Before quitting, we need to restore the user's backlighting settings
 		LogitechGSDK.LogiLedRestoreLighting();
      	LogitechGSDK.LogiLedShutdown();
